76827190e5ad26da9a56fa6f1744bd67ada04d74
Carlos Lugo <calugo71@gmail.com>
2022-11-21 11:01:12 -0700

    deployment.6

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://chuysbikerepairapi.onrender.com',
+    baseUrl: 'https://chuys-bike-repair-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
